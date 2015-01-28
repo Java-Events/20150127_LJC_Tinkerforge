60e0bf65b5b4ef87fbe25c6eec85f34c26030aed
Sven Ruppert <sven.ruppert@gmail.com>
2015-01-28 11:33:13 +0000

    added slides -> data/ v001 : AmbientLight writing to CommandLine

---
+++ data/slide_config.js
@@ -0,0 +1,36 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Industrial Prototyping - <br/> IoT / Java8 / TinkerForge',
+    eventTitle: 'LJC 27-28.01.2015',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/logo_sru.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ]//,
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Sven Ruppert',
+    company: 'Principal IT Consultant , codecentric AG - München',
+    gplus: 'https://www.google.com/+SvenRuppert',
+    twitter: '@SvenRuppert',
+    www: 'http://www.codecentric.de',
+    github: 'https://github.com/svenruppert'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
--- src/main/java/org/rapipdm/iot/ljc/tinkerforge/v001/Main001.java
+++ src/main/java/org/rapipdm/iot/ljc/tinkerforge/v001/Main001.java
@@ -1,12 +1,42 @@
 package org.rapipdm.iot.ljc.tinkerforge.v001;
 
+import com.tinkerforge.BrickletAmbientLight;
+import com.tinkerforge.IPConnection;
+
 /**
  * Created by sven on 27.01.15.
+ *
+ * Will write the Data on CommandLine
+ *
  */
 public class Main001 {
+    private static final String HOST = "localhost";
+    private static final int PORT = 4223;
+    private static final String UID = "mbL"; // Change to your UID
 
+    // Note: To make the example code cleaner we do not handle exceptions. Exceptions you
+    //       might normally want to catch are described in the documentation
+    public static void main(String args[]) throws Exception {
+        IPConnection ipcon = new IPConnection(); // Create IP connection
+        BrickletAmbientLight al = new BrickletAmbientLight(UID, ipcon); // Create device object
 
-    public static void main(String[] args) {
+        ipcon.connect(HOST, PORT); // Connect to brickd
+        // Don't use device before ipcon is connected
 
+        // Set Period for illuminance callback to 1s (1000ms)
+        // Note: The illuminance callback is only called every second if the
+        //       illuminance has changed since the last call!
+        al.setIlluminanceCallbackPeriod(1000);
+
+        // Add and implement illuminance listener (called if illuminance changes)
+        al.addIlluminanceListener(new BrickletAmbientLight.IlluminanceListener() {
+            public void illuminance(int illuminance) {
+                System.out.println("Illuminance: " + illuminance / 10.0 + " Lux");
+            }
+        });
+
+        System.out.println("Press key to exit");
+        System.in.read();
+        ipcon.disconnect();
     }
 }
