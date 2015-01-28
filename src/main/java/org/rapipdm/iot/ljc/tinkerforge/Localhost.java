acab06cff999e330b92e2c6b36f942552aca8095
Sven Ruppert <sven.ruppert@gmail.com>
2015-01-28 12:05:56 +0000

    v002 : added LCD20x4 Demo with AmbientLight

---
+++ src/main/java/org/rapipdm/iot/ljc/tinkerforge/Localhost.java
@@ -0,0 +1,10 @@
+package org.rapipdm.iot.ljc.tinkerforge;
+
+/**
+ * Created by Sven Ruppert on 01.02.14.
+ */
+public class Localhost {
+//    public static final String HOST = "192.168.0.103";
+    public static final String HOST = "localhost";
+    public static final int PORT = 4223;
+}
